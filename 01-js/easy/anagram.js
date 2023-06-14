7f27695982f30b71fccdd66644292885ef28bde2
Somdyuti <somdyuti.newton159@gmail.com>
2023-06-14 15:21:29 +0530

    Solved Anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,25 @@
 */
 
 function isAnagram(str1, str2) {
-
+  var n1=str1.length;
+  var n2=str2.length;
+  
+  if(n1!=n2) 
+    return false;
+  
+  const arr1 = str1.split("");
+  const arr2 = str2.split("");
+  arr1.sort();
+  arr2.sort();
+  
+  for(var i=0;i<n1;i++){
+    if(arr1[i]!=arr2[i]){
+      return false;
+    }
+  }
+  return true;
 }
+// var ans=isAnagram("somds","pomss");
+// console.log(ans);
 
 module.exports = isAnagram;
