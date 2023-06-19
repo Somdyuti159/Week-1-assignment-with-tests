8d06548f2fc4ba3bfc145541e6d92873838e7f45
Somdyuti <somdyuti.newton159@gmail.com>
2023-06-19 17:33:36 +0530

    Expenditure Analysis

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,70 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  var n=transactions.length;
+  var spendEstimates={};
+  //Iterate the objects in the array
+  for(var i=0;i<n;i++){
+    var t=transactions[i];//Store the objects in the variable
+    if(spendEstimates[t.category]){
+      spendEstimates[t.category]+=t.price;
+    }
+    else{
+      spendEstimates[t.category]=t.price;
+    }
+  }
+  let ans=[];
+  var key=Object.keys(spendEstimates);
+  for(var i=0;i<key.length;i++){
+    var obj={
+      category: key[i],
+      totalSpent: spendEstimates[key[i]]
+    }
+    ans.push(obj);
+  }
+  console.log(ans);
+  return ans; 
 }
 
+
+
+// const transactions = [
+//   {
+//     id: 1,
+//     timestamp: 1656076800000,
+//     price: 10,
+//     category: 'Food',
+//     itemName: 'Pizza',
+//   },
+//   {
+//     id: 2,
+//     timestamp: 1656259600000,
+//     price: 20,
+//     category: 'Food',
+//     itemName: 'Burger',
+//   },
+//   {
+//     id: 3,
+//     timestamp: 1656019200000,
+//     price: 15,
+//     category: 'Clothing',
+//     itemName: 'T-Shirt',
+//   },
+//   {
+//     id: 4,
+//     timestamp: 1656364800000,
+//     price: 30,
+//     category: 'Electronics',
+//     itemName: 'Headphones',
+//   },
+//   {
+//     id: 5,
+//     timestamp: 1656105600000,
+//     price: 25,
+//     category: 'Clothing',
+//     itemName: 'Jeans',
+//   },
+// ];
+
+// calculateTotalSpentByCategory(transactions);
 module.exports = calculateTotalSpentByCategory;
